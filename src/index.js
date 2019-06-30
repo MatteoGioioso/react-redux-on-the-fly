7f14c876bea3a85b0509eae299ec47c616e8509e
MatteoGioioso <gioioso.matteo@gmail.com>
2019-06-30 15:59:03 +0700

    Add additional exports

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import { createActions } from "./createActions";
 import { createNamedWrapperReducer } from "./helpers";
 import { baseArrayReducer } from "./baseArrayReducer";
 import { baseObjectReducer } from "./baseObjectReducer";
+import { asyncCombineReducers } from "./asyncCombineReducers";
+import { ReactReduxOnTheFlyProvider } from "./ReactReduxOnTheFlyContext";
 
 export {
+  ReactReduxOnTheFlyProvider,
+  asyncCombineReducers,
   withAsyncReducer,
   createActions,
   createNamedWrapperReducer,
