b7c5db937d613a9465ac5d730e95ea9f1774e937
MatteoGioioso <gioioso.matteo@gmail.com>
2019-06-30 12:13:49 +0700

    Fix rollup bug with some export in common.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { withAsyncReducer } from "./injectAsyncReducer";
+import { createActions } from "./createActions";
+import { createNamedWrapperReducer } from "./helpers";
+import { baseArrayReducer } from "./baseArrayReducer";
+import { baseObjectReducer } from "./baseObjectReducer";
+
+export {
+  withAsyncReducer,
+  createActions,
+  createNamedWrapperReducer,
+  baseArrayReducer,
+  baseObjectReducer
+};
