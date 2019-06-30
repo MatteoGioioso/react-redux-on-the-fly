89ffaca92884f74836a7304e59adc0febbc69ee5
MatteoGioioso <gioioso.matteo@gmail.com>
2019-06-30 11:25:14 +0700

    Now you can pass an array of identifier to injectAsyncReducer

--- src/injectAsyncReducer.js
+++ src/injectAsyncReducer.js
@@ -1,19 +1,18 @@
 import { ReactReduxContext } from "react-redux";
 import React from "react";
-
-/**
- * withAsyncReducer
- * HOC to inject a reducer into the store dynamically
- * @param namespace
- * @param identifierName id or any other unique identifier
- * @param anonymousReducer a reducer without name, the name will be generated on component injection
- */
 import {
   asyncReducerManager,
   getReducerName,
   injectAsyncReducer
 } from "./helpers";
 
+/**
+ * withAsyncReducer
+ * HOC to inject a reducer into the store dynamically
+ * @param {string} namespace
+ * @param {string | string[]} identifierName id or any other unique identifier
+ * @param anonymousReducer a reducer without name, the name will be generated on component injection
+ */
 export const withAsyncReducer = (
   namespace,
   identifierName,
@@ -31,49 +30,6 @@ export const withAsyncReducer = (
           asyncReducerManager(store, props);
 
           return <WrappedComponent {...props} reducerName={reducerName} />;
-        }}
-      </ReactReduxContext.Consumer>
-    );
-  };
-};
-
-/**
- * withAsyncReducer
- * HOC to inject a reducer into the store dynamically
- * @param {Array} namespaces
- * @param {Array} identifierNames id or any other unique identifier
- * @param {Array} reducers a reducer without name, the name will be generated on component injection
- */
-export const withMultipleAsyncReducers = (
-  namespaces,
-  identifierNames,
-  reducers
-) => WrappedComponent => {
-  return props => {
-    const reducersNames = namespaces.map((namespace, index) =>
-      getReducerName(props, identifierNames[index], namespace)
-    );
-
-    return (
-      <ReactReduxContext.Consumer>
-        {({ store }) => {
-          reducersNames.forEach((reducerName, index) =>
-            injectAsyncReducer(store, reducerName, reducers[index](reducerName))
-          );
-
-          asyncReducerManager(store, props);
-
-          const mapArrayToObject = () =>
-            Object.assign(
-              {},
-              ...namespaces.map((namespace, index) => ({
-                [namespace]: reducersNames[index]
-              }))
-            );
-
-          return (
-            <WrappedComponent {...props} reducerNames={mapArrayToObject()} />
-          );
         }}
       </ReactReduxContext.Consumer>
     );
