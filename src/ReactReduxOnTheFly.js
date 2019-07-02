9fb8a186e40a5d7eacb7bfd2ee727b043337f286
Matteo <gioioso.matteo@gmail.com>
2019-07-02 08:24:06 +0700

    Add class for ReactReduxOnTheFly
    Some methods needs to be instanciated with static reducers

---
+++ src/ReactReduxOnTheFly.js
@@ -0,0 +1,34 @@
+import { combineReducers } from "redux";
+
+export default class ReactReduxOnTheFly {
+  constructor(staticReducers, options) {
+    this._staticReducers = staticReducers;
+    this._options = options;
+    this.MAX_ASYNC_REDUCERS_STACK = 30;
+  }
+
+  asyncCombineReducers(asyncReducers) {
+    return combineReducers({
+      ...this._staticReducers,
+      ...asyncReducers
+    });
+  }
+
+  asyncReducerManager(store) {
+    const asyncReducersStack = Object.keys(store.asyncReducers);
+
+    if (asyncReducersStack.length > this.MAX_ASYNC_REDUCERS_STACK) {
+      this.ejectAsyncReducer(store, asyncReducersStack[0]);
+    }
+  }
+
+  injectAsyncReducer(store, name, asyncReducer) {
+    store.asyncReducers[name] = asyncReducer;
+    store.replaceReducer(this.asyncCombineReducers(store.asyncReducers));
+  }
+
+  ejectAsyncReducer(store, name) {
+    delete store.asyncReducers[name];
+    store.replaceReducer(this.asyncCombineReducers(store.asyncReducers));
+  }
+}
